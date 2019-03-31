d0156788abcdc9c8e2b745e784ac22bfe77d5cf8
hongzhijie <hzj_777@163.com>
2019-03-31 09:29:54 +0800

    修改打包方式为jar

--- src/main/java/com/smallmall/SmallmallApplication.java
+++ src/main/java/com/smallmall/SmallmallApplication.java
@@ -3,10 +3,7 @@ package com.smallmall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /*
@@ -19,13 +16,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  **/
 @SpringBootApplication
 //启用线程池
-@EnableAsync
+//@EnableAsync
 //启用缓存
 @EnableCaching
 //开启事务支持
 @EnableTransactionManagement
 @MapperScan("com.smallmall.dao")
-public class SmallmallApplication extends SpringBootServletInitializer {
+public class SmallmallApplication {
+//        extends SpringBootServletInitializer
     public static void main(String[] args) {
         SpringApplication.run(SmallmallApplication.class, args);
         System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
@@ -38,10 +36,10 @@ public class SmallmallApplication extends SpringBootServletInitializer {
     }
 
 
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(SmallmallApplication.class);
-    }
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类
+//        return builder.sources(SmallmallApplication.class);
+//    }
 
 }
 
